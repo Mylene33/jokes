59c2e67e86334eb2589d94ea3aa3e2a859558216
Mylene CARRARA <mylene.carrara33@gmail.com>
2021-01-16 18:26:51 +0100

    commit 2

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import QuoteCard from './components/QuoteCard';
+
+function App() {
+  return (
+    <div>
+      <QuoteCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QuoteCard.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './QuoteCard.css';
+
+class QuoteCard extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            joke:null
+        }
+    }
+
+    handleClick = () => {
+        fetch('https://simpsons-quotes-api.herokuapp.com/quotes')
+        .then((resp) => resp.json())
+        .then((data) => this.displayJoke(data)); 
+    };
+
+    displayJoke = (joke) => {
+        this.setState({joke:joke}); 
+    }
+
+    render(){
+        return(
+            <>
+                <button onClick={this.handleClick}>Une nouvelle blague!</button>
+
+                {
+                    this.state.joke != null && 
+
+                    <figure className="QuoteCard">
+                    <img src={this.state.joke[0].image} alt={this.state.joke[0].character} />
+                    <figcaption>
+                    <blockquote>{this.state.joke[0].quote}</blockquote>
+                    <cite>{this.state.joke[0].character}</cite>
+                    </figcaption>
+                    </figure>
+                        
+                }
+            </>
+        )
+    }
+}
+
+export default QuoteCard;
